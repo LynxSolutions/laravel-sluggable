c828b204388e4cb8130043e0e9284c47e9cbbc7e
kukukk <kukukk+github@pm.me>
2020-09-03 18:13:33 +0300

    Revert PHP 7.4 specific code

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Str;
 
 trait HasSlug
 {
-    protected SlugOptions $slugOptions;
+    protected $slugOptions;
 
     abstract public function getSlugOptions(): SlugOptions;
 
@@ -93,7 +93,9 @@ trait HasSlug
         }
 
         $slugSourceString = collect($this->slugOptions->generateSlugFrom)
-            ->map(fn (string $fieldName): string => data_get($this, $fieldName, ''))
+            ->map(function (string $fieldName): string {
+                return data_get($this, $fieldName, '');
+            })
             ->implode($this->slugOptions->slugSeparator);
 
         return $this->generateSubstring($slugSourceString);
@@ -121,7 +123,7 @@ trait HasSlug
         $key = $this->getKey();
 
         if ($this->getIncrementing()) {
-            $key ??= '0';
+            $key = $key ?? '0';
         }
 
         $query = static::where($this->slugOptions->slugField, $slug)
